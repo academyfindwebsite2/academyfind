083fb0e310d0154a088623d33d11b7404c1c809f
Monish <monishmathurt@gmail.com>
2026-06-13 23:46:45 +0530

    feat: Fix logic for determining needClaim in UserCreateInstitutePage

--- app/user/create-institute/page.tsx
+++ app/user/create-institute/page.tsx
@@ -38,7 +38,7 @@ export default async function UserCreateInstitutePage() {
                     instituteId: latestReq.instituteId
                 }
             })
-            if(existingClaim){
+            if(!existingClaim){
                 needClaim = true;
             }
         }
