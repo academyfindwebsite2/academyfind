2f1a0ee415e0124691ed4de7b2de5c5d59799cc0
Monish <monishmathurt@gmail.com>
2026-07-02 19:47:35 +0530

    changed grid cols-3 to cols-2 for author posts page to make it look better on mobile and desktop.

--- components/blog/author/AuthorPosts.tsx
+++ components/blog/author/AuthorPosts.tsx
@@ -9,7 +9,7 @@ export default function AuthorPostGrid({
   posts,
 }: AuthorPostGridProps) {
   return (
-    <div className="grid gap-8 sm:grid-cols-2 xl:grid-cols-3">
+    <div className="grid gap-8 sm:grid-cols-2 xl:grid-cols-2">
       {posts.map((post: BlogCardPost) => (
         <BlogCard
           key={post.id}
